3b123e0f6b4e5036b79fc3ad0c65e6624ff83629
Wilson Alves <wilrrama@hotmail.com>
2021-03-04 11:22:02 -0300

    Add Admin navbar

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,13 @@
+import Pagination from 'core/components/Pagination';
 import React from 'react';
+import Navbar from './components/Navbar';
+import './styles.scss';
 
 const Admin = () => (
 <div>
-    <h1> Admin </h1>
+     <Navbar/>
 </div>
+
 );
 
 export default Admin;
